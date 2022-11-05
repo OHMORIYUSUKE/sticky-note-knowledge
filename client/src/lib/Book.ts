da651efc12e4cb52f16727de005a889b0b69de67
OHMORIYUSUKE <omoriyusuke@gmail.com>
2022-11-06 03:24:38 +0900

    booklist page

---
+++ client/src/lib/Book.ts
@@ -0,0 +1,6 @@
+export type Book = {
+  id: string;
+  title: string;
+  text: string;
+  createdAt: Date;
+};
---
+++ client/src/lib/StickyNote.ts
@@ -0,0 +1,10 @@
+export type StickyNote = {
+  id: string;
+  text: string;
+  color: string;
+  userId: string;
+  bookId: string;
+  yCoordinates: number;
+  xCoordinates: number;
+  createdAt: Date;
+};
