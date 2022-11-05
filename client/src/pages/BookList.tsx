3011fb649214d9f152b6648c89d3002c4b7c9c06
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-11-06 04:08:44 +0900

    style: BookListのUIを作成

--- client/src/pages/BookList.tsx
+++ client/src/pages/BookList.tsx
@@ -1,4 +1,4 @@
-import { TextArea, HStack, Box } from "native-base";
+import { TextArea, HStack, Box, Center, Heading, Image } from "native-base";
 import { FlatList, Text, StatusBar, SectionList, View } from "react-native";
 import { book } from "@prisma/client";
 import axios from "axios";
@@ -19,40 +19,49 @@ const BookList = () => {
       });
   }, []);
 
-  const DATA = [
-    {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"],
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"],
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"],
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"],
-    },
-  ];
-
-  const Item = ({ title }: any) => (
-    <View>
-      <Text>{title}</Text>
-    </View>
-  );
   return (
     <>
-      <Text>読みたい本を選択</Text>
-
-      <SectionList
-        sections={DATA}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({ item }) => <Item title={item} />}
-        renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
-      />
+      <Center>
+        <Heading size="xl" p={20}>
+          読みたい本を選択
+        </Heading>
+        <FlatList
+          scrollEnabled={false}
+          data={books}
+          keyExtractor={(book) => book.id}
+          numColumns={3}
+          renderItem={({ item }) => (
+            <View>
+              {/* <Image source={{ uri: item.url }} style={styles.image} /> */}
+              <Box
+                height="200"
+                shadow="2"
+                rounded="lg"
+                _dark={{
+                  bg: "coolGray.200:alpha.20",
+                }}
+                _light={{
+                  bg: "coolGray.200:alpha.20",
+                }}
+              >
+                <Image
+                  shadow="2"
+                  rounded="lg"
+                  size={100}
+                  m={2}
+                  source={{
+                    uri: "https://static.es.cyberowl.jp/images/article/original/5e5864c561a30.jpg?7a0cb68&w=680",
+                  }}
+                  alt="Alternate Text"
+                />
+                <Heading size="lg" p={2}>
+                  {item.title}
+                </Heading>
+              </Box>
+            </View>
+          )}
+        />
+      </Center>
     </>
   );
 };
