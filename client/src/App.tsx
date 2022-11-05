325e992cfce152939c3b5cdd4168d5bfec4ffc65
yud0uhu <cist.b219.y.osaka@gmail.com>
2022-11-06 00:54:32 +0900

    fix: react-native-paperを削除

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { trpc } from "./trpc";
 import { NativeBaseProvider } from "native-base";
 import { useState } from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
 
 import Header from "./Header";
 import Top from "./pages/BookView";
@@ -14,18 +13,16 @@ const url = "http://localhost:3000/trpc";
 const App = () => {
   const [trpcClient] = useState(() => trpc.createClient({ url }));
   return (
-    <PaperProvider>
-      <NativeBaseProvider>
-        <trpc.Provider queryClient={client} client={trpcClient}>
-          <QueryClientProvider client={client}>
-            <Header />
-            <SafeAreaView style={styles.container}>
-              <BookView />
-            </SafeAreaView>
-          </QueryClientProvider>
-        </trpc.Provider>
-      </NativeBaseProvider>
-    </PaperProvider>
+    <NativeBaseProvider>
+      <trpc.Provider queryClient={client} client={trpcClient}>
+        <QueryClientProvider client={client}>
+          <Header />
+          <SafeAreaView style={styles.container}>
+            <BookView />
+          </SafeAreaView>
+        </QueryClientProvider>
+      </trpc.Provider>
+    </NativeBaseProvider>
   );
 };
 
